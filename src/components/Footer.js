5447141cfd7b6e72f5cdd3d40ac5a23e5699ca4a
QuintonJason <quinton.jason@gmail.com>
2018-07-05 17:34:03 -0500

    added footer and styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-const BlogLink = (props) => {
+const Footer = (props) => {
   return (
-    <footer className="post-landing-link">
-      <h1>This is the footer</h1>
+    <footer className="footer">
+      <div className="footer-mailing">
+        <div className="footer-mailing__heading">
+          <h2 className="">Join our mailing list</h2>
+          <p className="">Get event updates as we get closer to the big event.</p>
+        </div>
+        <form className="footer-mailing__form">
+          <label className="t-hidden">Your email</label>
+          <input type="email" id="mce-EMAIL" className="footer-mailing__input" name="EMAIL" placeholder="Your email" value=""/>
+          <input type="submit" className="footer-mailing__submit" />
+        </form>
+      </div>
+      <div className="footer-register">
+        <h2 className="t-section-headline footer-register__headline">Activate with us</h2>
+        <a href="#0" className="btn btn-lg footer-register__link">Register Today</a>
+      </div>
     </footer>
   );
 };
 
-export default BlogLink;
+export default Footer;

8422e6db79a1d5004beb7e931b2a2e445a500781
QuintonJason <quinton.jason@gmail.com>
2018-07-08 10:12:26 -0500

    added updates component

---
+++ src/pages/updates.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const Updates = (props) => {
+  return (
+    <div className="updates">
+      <h2>This is the Updates component</h2>
+    </div>
+  );
+};
+
+export default Updates;

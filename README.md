341d040d4598140c8e2d0b28ee38d42831383cf9
QuintonJason <quinton.jason@gmail.com>
2018-07-09 20:42:46 -0500

    readme and comment

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ This will be the Gatsby boilerplate for future Activate Sites
 ## Roadmap
  - [ ] update type scale
  - [ ] verify all production fonts
- - [ ] create cards css
+ - [x] create cards css
  - [x] create json file for speaker data
  - [x] create json file for sponsor data
  - [x] show speaker data on workshow / conference page
  - [x] show sponsor data on sponsor page
  - [ ] create component / model for page headers
+ - [ ] create stateful components for speakers/workshops
 
 ## Running in development
 `gatsby develop`
--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -5,8 +5,12 @@ const Speaker = props => {
   return (
     <li className="card card-speaker">
       <div className="card-speaker__info">
-        <h3 className="t-section-headline card__headline">{props.name}</h3>
+        <h3 className="t-section-headline card__headline card-speaker__headline">{props.name}</h3>
         <p className="t-body card__title">{props.title} </p>
+       {/* Q TODO
+          update the component to be conditional
+          if student, text == "Student Track"
+          if professional, text == "Professional Track" */}
         <span className="t-body card__speaker-track">{props.track}</span>
         <p className="t-body card__time">{props.slot}</p>
       </div>
